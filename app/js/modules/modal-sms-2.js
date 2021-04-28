55b91565418d0cb60fd9ea143508ec8d5d376f19
Luwanhika <luwanhika@hotmail.com>
2021-04-28 20:49:35 -0300

    atualizar overley

--- app/js/modules/modal-sms-2.js
+++ app/js/modules/modal-sms-2.js
@@ -2,8 +2,9 @@ export default function initModalSms() {
     const botaoEnviar = document.querySelector('[data-reenviar="abrir"]');
     const botaoFechar = document.querySelector('[data-reenviar="fechar"]');
     const containerModal = document.querySelector('[data-reenviar="container"]');
+    const botaoAutorizar = document.querySelector('[data-autorizar="abrir"]');
 
-    if (botaoEnviar && botaoFechar && containerModal) {
+    if (botaoEnviar && botaoFechar && containerModal && botaoAutorizar) {
 
         function toggleModal(event) {
             event.preventDefault();
@@ -19,5 +20,6 @@ export default function initModalSms() {
         botaoEnviar.addEventListener('click', toggleModal);
         botaoFechar.addEventListener('click', toggleModal);
         containerModal.addEventListener('click', cliqueForaModal);
+        botaoAutorizar.addEventListener('click', toggleModal);
     }
 }
